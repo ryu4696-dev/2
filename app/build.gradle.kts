d7bf4348d98c0336fdf848cd00b8125584433ffe
ryu4696-dev <ryu4696@gmail.com>
2026-09-13 00:56:53 +0900

    Bump app to v5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "dev.ryu4696.hitandblow"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "4.0"
+        versionCode = 5
+        versionName = "5.0"
     }
 
     buildTypes {
