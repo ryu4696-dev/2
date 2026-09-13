47f890dc5cf07964d9255275fbbcec68c2059aee
ryu4696-dev <ryu4696@gmail.com>
2026-09-13 22:47:15 +0900

    v16: bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "dev.ryu4696.hitandblow"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "15.0"
+        versionCode = 16
+        versionName = "16.0"
     }
 
     buildTypes {
