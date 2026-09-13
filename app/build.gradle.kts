4651b74db353f47be89f57ea8a55db21bf704614
ryu4696-dev <ryu4696@gmail.com>
2026-09-13 20:22:33 +0900

    v13: make answer lid fully opaque

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "dev.ryu4696.hitandblow"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "12.0"
+        versionCode = 13
+        versionName = "13.0"
     }
 
     buildTypes {
