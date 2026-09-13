7f1c83c6e9e97d45765537c5a7832ad6f8dca63f
ryu4696-dev <ryu4696@gmail.com>
2026-09-13 21:57:38 +0900

    v15: bump app version for shared-board multiplayer

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "dev.ryu4696.hitandblow"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "14.0"
+        versionCode = 15
+        versionName = "15.0"
     }
 
     buildTypes {
